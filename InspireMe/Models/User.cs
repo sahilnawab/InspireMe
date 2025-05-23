9fb9eca5e28eebe8817df44502e3179af062b5cd
sahil nawab <nawabsahil17@gmail.com>
2025-05-23 20:07:11 +0530

    add User

---
+++ InspireMe/Models/User.cs
@@ -0,0 +1,10 @@
+﻿namespace InspireMe.Models
+{
+    public class User
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string email { get; set; }
+        public string passowrd { get; set; }
+    }
+ }
