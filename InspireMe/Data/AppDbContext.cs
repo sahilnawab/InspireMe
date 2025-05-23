0d9b98f2d883555e485e708b1d41cf8c441d70c8
sahil nawab <nawabsahil17@gmail.com>
2025-05-23 18:51:30 +0530

    Add project files.

---
+++ InspireMe/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using InspireMe.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace InspireMe.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Quote> Quotes { get; set; }
+    }
+}
---
+++ InspireMe/Models/Quote.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace InspireMe.Models
+{
+    public class Quote
+    {
+        [Key]
+        public int id { get; set; }
+        public string author { get; set; }
+        public string content { get; set; }
+
+    }
+}
